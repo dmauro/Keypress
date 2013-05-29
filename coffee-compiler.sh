86a03c9076b36a2ba81bbe6ce0e33be2eef3aaef
dmauro <dave@canv.as>
2013-05-29 10:40:54 -0400

    Add compiler to the repo.

---
+++ coffee-compiler.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ~/Projects/Keypress
+coffee -c keypress.coffee
+java -jar compiler.jar --js keypress.js --js_output_file keypress.min.js
+rm keypress.js
+mv keypress.min.js keypress.js
+echo "/* Keypress version 1.0.5 */"|cat - keypress.js > /tmp/out && mv /tmp/out keypress.js
